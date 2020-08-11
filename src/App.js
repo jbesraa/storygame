e470dda158ef540616afeb54269d4ad54a2a1d7c
Esraa Jbara <jbesraa@gmail.com>
2020-08-12 01:47:40 +0300

    move card funcs to card folder

--- src/App.js
+++ src/App.js
@@ -1,63 +1,13 @@
 import React from 'react';
-import {View, Button} from 'react-native-ui-lib';
-import {StyleSheet} from 'react-native';
+import {View, Text} from 'react-native-ui-lib';
+import Players from './players';
 import Card from './card';
-import {randomInteger} from './utils';
-import CardsData from './data';
-
-const styles = StyleSheet.create({
-  btnWrapper: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingTop: 20,
-  },
-  btn: {
-    width: '50%',
-  },
-});
 
 const App = () => {
-  const [visitedCards, setVisitedCards] = React.useState([]);
-  const [currentCard, setCurrentCard] = React.useState({name: '', imgURL: ''});
-  const [notVisitedCards, setNotVisitedCards] = React.useState([...CardsData]);
-  console.log('App -> notVisitedCards', notVisitedCards);
-
-  const handleOnPress = () => {
-    const cardsDataCount = notVisitedCards.length;
-    if (cardsDataCount < 1) {
-      console.log('done');
-      return;
-    }
-    const randomNumber = randomInteger(0, cardsDataCount - 1);
-    console.log('handleOnPress -> randomNumber', randomNumber);
-    const card = notVisitedCards[randomNumber];
-    console.log('handleOnPress -> card', card);
-    const {id, name, imgURL} = card;
-    setCurrentCard({name, imgURL});
-    setVisitedCards([...visitedCards, {id}]);
-    setNotVisitedCards(notVisitedCards.filter((c, i) => i !== randomNumber));
-    console.log(
-      'handleOnPress -> notVisitedCards.splice(randomNumber, 1)',
-      notVisitedCards.splice(randomNumber, 1),
-    );
-  };
-  console.log('App -> visitedCards', visitedCards);
   return (
-    <>
-      <View>
-        <Card name={currentCard.name} imgURL={currentCard.imgURL} />
-        <View style={styles.btnWrapper}>
-          <Button
-            label="Roll The Dice"
-            style={styles.btn}
-            body
-            onPress={handleOnPress}
-            bg-primaryColor
-            square
-          />
-        </View>
-      </View>
-    </>
+    <View>
+      <Players />
+    </View>
   );
 };
 
--- src/card/index.js
+++ src/card/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import {Button} from 'react-native-ui-lib';
+
 import {View, Text, Image, StyleSheet} from 'react-native';
+import {randomInteger} from '../utils';
+import CardsData from '../data';
 
 const styles = StyleSheet.create({
   container: {
@@ -8,25 +12,66 @@ const styles = StyleSheet.create({
     padding: 50,
   },
   img: {
-    width: '90%',
-    height: '80%',
+    width: '50%',
+    height: '40%',
   },
   text: {
     paddingBottom: 30,
     fontSize: 28,
   },
+  btnWrapper: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 100,
+  },
+  btn: {
+    width: '50%',
+  },
 });
 
-const Card = ({name, imgURL}) => {
+const Card = () => {
+  const [visitedCards, setVisitedCards] = React.useState([]);
+  const [currentCard, setCurrentCard] = React.useState({name: '', imgURL: ''});
+  const [notVisitedCards, setNotVisitedCards] = React.useState([...CardsData]);
+
+  const handleOnPress = () => {
+    const cardsDataCount = notVisitedCards.length;
+    if (cardsDataCount < 1) {
+      return;
+    }
+    const randomNumber = randomInteger(0, cardsDataCount - 1);
+
+    const {id, name, imgURL} = notVisitedCards[randomNumber];
+    setCurrentCard({name, imgURL});
+    setVisitedCards([...visitedCards, {id}]);
+    setNotVisitedCards(notVisitedCards.filter((c, i) => i !== randomNumber));
+  };
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>{name}</Text>
-      <Image
-        style={styles.img}
-        source={{
-          uri: `${imgURL}`,
-        }}
-      />
+    <View>
+      {currentCard.name ? (
+        <Text style={styles.text}>{currentCard.name}</Text>
+      ) : null}
+      {currentCard.imgURL ? (
+        <Image
+          style={styles.img}
+          source={{
+            uri: `${currentCard.imgURL}`,
+          }}
+        />
+      ) : null}
+
+      <View>
+        <View style={styles.btnWrapper}>
+          <Button
+            label="Roll The Dice"
+            style={styles.btn}
+            body
+            onPress={handleOnPress}
+            bg-primaryColor
+            square
+          />
+        </View>
+      </View>
     </View>
   );
 };

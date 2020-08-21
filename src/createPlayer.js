d2ba644d1f2cd49d1e1d78da1e12f352d332ac15
Esraa Jbara <jbesraa@gmail.com>
2020-08-22 00:57:34 +0300

    add create player template

---
+++ src/createPlayer.js
@@ -0,0 +1,19 @@
+const generatePlayerLogo = () => {
+  return;
+};
+
+const generatePlayerName = () => {
+  return;
+};
+
+const createPlayer = () => {
+  const name = generatePlayerName();
+  const logo = generatePlayerLogo();
+
+  return {
+    name,
+    logo,
+  };
+};
+
+export default createPlayer;

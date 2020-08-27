5f66ad319ed1aeedc7ec4aa181b9d7d96ec971a7
Esraa Jbara <jbesraa@gmail.com>
2020-08-27 20:25:13 +0300

    test createplayer fn

--- src/utils/index.spec.js
+++ src/utils/index.spec.js
@@ -1,5 +1,20 @@
-import {playersNames} from './index';
+import {createPlayer, playersNames} from './index';
 
-test('player names has the correct length', () => {
-  expect(playersNames).toHaveLength(6);
+describe('playerNames', () => {
+  test('player names has the correct length', () => {
+    expect(playersNames).toHaveLength(6);
+  });
+});
+
+describe('createPlayer', () => {
+  test('creates player successfuly', () => {
+    const names = ['testme'];
+    const result = createPlayer(names);
+    expect(result.name).toEqual('testme');
+  });
+  test('returns empty name and cards array for no names list', () => {
+    const names = [];
+    const result = createPlayer(names);
+    expect(result.name).toEqual('');
+  });
 });
